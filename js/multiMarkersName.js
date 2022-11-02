07869492ee648fbe68805df5707bc6da7d59c753
josephyoung81 <117132554+josephyoung81@users.noreply.github.com>
2022-11-01 22:18:42 -0500

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -59,3 +59,13 @@ AFRAME.registerComponent('registerevents', {
 			});
 		},
 	});
+
+
+var textEl = document.createElement('a-entity');
+    
+textEl.setAttribute('id','text');
+textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
+textEl.object3D.position.set(0, 0.7, 0);
+textEl.object3D.rotation.set(-90, 0, 0);
+
+markerEl.appendChild(textEl);
